0a34a6b14d0492e73b4e3ef2a395d88817911e48
Jonas Kabelitz <jonas-kabelitz@gmx.de>
2021-05-30 19:11:14 +0200

    added anti aliasing

--- sandbox/core/core.cpp
+++ sandbox/core/core.cpp
@@ -71,6 +71,7 @@ bool initialize() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_SAMPLES, 4);
 
   GLFWmonitor* primary_monitor = glfwGetPrimaryMonitor();
 
@@ -216,7 +217,7 @@ bool initialize() {
     *_mesh_atlas["monke"],
     {
       _texture_atlas["wooden_planks"],
-      _texture_atlas["blank"],
+      _texture_atlas["filled"],
     },
     {
       glm::vec3(-3.0f, 0.0f, 1.0f),
@@ -281,6 +282,7 @@ bool initialize() {
     }
   ));
 
+  glEnable(GL_MULTISAMPLE);
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LESS);
 
