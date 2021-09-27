c3c123bbdd9cb2e3bbd8e3d6690c1933ccb2e87f
Jonas Kabelitz <j-kabelitz@gmx.de>
2021-09-27 11:42:40 +0200

    Create scheduler.hpp

---
+++ sandbox/ecs/scheduler.hpp
@@ -0,0 +1,13 @@
+#ifndef SBX_ECS_SCHEDULER_HPP_
+#define SBX_ECS_SCHEDULER_HPP_
+
+namespace sbx {
+  
+template<typename Delta>
+class scheduler {
+
+}; // class scheduler
+  
+} // namespace sbx
+
+#endif // SBX_ECS_SCHEDULER_HPP_
