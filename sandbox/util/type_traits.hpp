588b335bcd965fdfdefc195904a9e4a28e430d20
Jonas Kabelitz <j-kabelitz@gmx.de>
2021-09-28 13:34:26 +0200

    Update type_traits.hpp

--- sandbox/util/type_traits.hpp
+++ sandbox/util/type_traits.hpp
@@ -16,6 +16,15 @@ struct constness_as<const From, To> {
 template<typename From, typename To>
 using constness_as_t = typename constness_as<From, To>::type;
     
+template<typename Type>
+[[nodiscard]] constexpr auto to_address(Type &&ptr) noexcept {
+  if constexpr(std::is_pointer_v<std::remove_const_t<std::remove_reference_t<Type>>>) {
+    return ptr;
+  } else {
+    return to_address(std::forward<Type>(ptr).operator->());
+  }
+}
+    
 } // namespace sbx
 
 #endif // SBX_UTIL_TYPE_TRAITS_HPP_
