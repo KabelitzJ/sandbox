1242f61a8f84122eeb4d703ccb0eb77af54606b6
Jonas Kabelitz <j-kabelitz@gmx.de>
2021-09-28 12:29:47 +0200

    Update registry.cpp

--- sandbox/ecs/registry.cpp
+++ sandbox/ecs/registry.cpp
@@ -1,23 +1,7 @@
 #include "registry.hpp"
 
-#include <cassert>
-
 namespace sbx {
 
-registry::registry() {
-    
-}
 
-registry::~registry() {
-  
-}
-
-entity registry::create_entity() {
-  return entity{0};
-}
-
-void registry::destoy_entity(entity entity) {
-  (void)entity;
-}
 
 } // namespace sbx
