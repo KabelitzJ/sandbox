7d68ecedda7cb59d2bb4cff4f814966af269cd0c
Jonas Kabelitz <j-kabelitz@gmx.de>
2021-10-06 10:11:34 +0200

    Update type_traits.hpp

--- sandbox/util/type_traits.hpp
+++ sandbox/util/type_traits.hpp
@@ -38,7 +38,28 @@ struct is_iterator_type<Type, Iterator, std::enable_if_t<!std::is_same_v<Type, I
 
 template<typename Type, typename Iterator>
 inline constexpr bool is_iterator_type_v = is_iterator_type<Type, Iterator>::value;
-    
+
+
+template<typename... Types>
+struct type_list {
+  using type = type_list;
+  static constexpr auto size = sizeof...(Types);
+};
+
+template<std::size_t, typename>
+struct type_list_element;
+
+template<std::size_t Index, typename Type, typename... Others>
+struct type_list_element<Index, type_list<Type, Others...>> : type_list_element<Index - 1u, type_list<Others...>> {};
+
+template<typename Type, typename... Others>
+struct type_list_element<0u, type_list<Type, Others...>> {
+  using type = Type;
+};
+
+template<std::size_t Index, typename List>
+using type_list_element_t = typename type_list_element<Index, List>::type;
+
 
 template<typename Type>
 [[nodiscard]] constexpr auto to_address(Type &&ptr) noexcept {
