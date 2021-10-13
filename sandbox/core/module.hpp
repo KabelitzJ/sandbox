d78931df9baa04abe6d532a9ad00a062b24c9702
Jonas Kabelitz <j-kabelitz@gmx.de>
2021-10-13 13:23:31 +0200

    Update module.hpp

--- sandbox/core/module.hpp
+++ sandbox/core/module.hpp
@@ -21,19 +21,29 @@ public:
 
 protected:
   template<typename System, typename... Args>
-  void _add_system(Args&&... args) {
+  void add_system(Args&&... args) {
     static_assert(std::is_base_of_v<basic_system<System, fast_time>, System>);
     static_assert(!std::is_abstract_v<System>);
 
-    scheduler->attach<System>(std::forward<Args>(args)...);
+    _scheduler->attach<System>(std::forward<Args>(args)...);
   }
-
-  static scheduler* _scheduler;
-  static event_queue* _event_queue;
+  
+  template<typename Event, typename Listener>
+  void add_listener(Listener&& listener) {
+     _event_queue->add_listener<Event>(std::forward<Listener>(listener));
+  }
+  
+  template<typename Event, typename... Args>
+  void emplace_event(Args&&... args) {
+     _event_queue->emplace<Event>(std::forward<Args>(args)...);
+  }
   
 private:
   friend class engine;
 
+  static scheduler* _scheduler;
+  static event_queue* _event_queue;
+  
 }; // class module
 
 } // namespace sbx
