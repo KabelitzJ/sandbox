3eecd2a693157b948185490cf1d8855878641b96
Jonas Kabelitz <j-kabelitz@gmx.de>
2021-10-13 13:13:59 +0200

    Update module.hpp

--- sandbox/core/module.hpp
+++ sandbox/core/module.hpp
@@ -28,8 +28,8 @@ protected:
     scheduler->attach<System>(std::forward<Args>(args)...);
   }
 
-  static ::sbx::scheduler* scheduler;
-  static ::sbx::event_queue* event_queue;
+  static scheduler* _scheduler;
+  static event_queue* _event_queue;
   
 private:
   friend class engine;
