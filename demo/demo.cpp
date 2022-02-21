3d100dbde3477ac5c1f69520c051e2383dd55f1b
KabelitzJ <jonas-kabelitz@gmx.de>
2022-02-21 21:05:54 +0100

    basic_matrix_

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -8,5 +8,8 @@
 struct foo { };
 
 int main() {
+
+  auto set = sbx::sparse_set<sbx::uint32>{};
+
   return 0;
 }
--- sandbox/container/sparse_set.hpp
+++ sandbox/container/sparse_set.hpp
@@ -1,11 +1,27 @@
 #ifndef SBX_CONTAINER_SPARSE_SET_HPP_
 #define SBX_CONTAINER_SPARSE_SET_HPP_
 
+#include <bit>
+#include <concepts>
+#include <vector>
+#include <memory>
+
+#include <meta/concepts.hpp>
+
 namespace sbx {
 
-class sparse_set {
+template<std::unsigned_integral Type, allocator<Type> Allocator, std::size_t PageSize>
+requires (std::has_single_bit(PageSize))
+class basic_sparse_set {
 
-};
+public:
+
+private:
+
+}; // class basic_sparse_set
+
+template<std::unsigned_integral Type>
+using sparse_set = basic_sparse_set<Type, std::allocator<Type>, std::size_t{4096}>;
 
 } // namespace sbx
 
