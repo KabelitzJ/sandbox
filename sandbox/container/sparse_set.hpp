6acab9c1877348b54270a6c18285cf79b9d89215
KabelitzJ <jonas-kabelitz@gmx.de>
2022-02-21 20:40:04 +0100

    ds

---
+++ sandbox/container/sparse_set.hpp
@@ -0,0 +1,14 @@
+#ifndef SBX_CONTAINER_SPARSE_SET_HPP_
+#define SBX_CONTAINER_SPARSE_SET_HPP_
+
+namespace sbx {
+
+class sparse_set {
+
+};
+
+} // namespace sbx
+
+#include "sparse_set.inl"
+
+#endif // SBX_CONTAINER_SPARSE_SET_HPP_
