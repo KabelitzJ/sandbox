ca8383671575fb7e3563461d40648558c7c64a39
KabelitzJ <jonas-kabelitz@gmx.de>
2022-11-03 13:59:12 +0100

    kdjlasdj

--- demo/demo/demo.cpp
+++ demo/demo/demo.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
-#include <libsbx/core/version.hpp>
+#include <libsbx/core/core.hpp>
+
+class test_module : public sbx::core::module<test_module> {
+  
+  inline static const auto registered = register_module(stage::normal);
+
+public:
+
+  test_module() = default;
+  ~test_module() override = default;
+
+  void update() override {
+    std::cout << "test_module::update()\n";
+  }
+
+private:
+
+}; // class test_module
 
 int main() {
   std::cout << "libsbx-core: " << LIBSBX_CORE_VERSION_STR << "\n";
+
+  sbx::core::module_manager::create_all();
+
+  sbx::core::module_manager::update_stages();
+
+  sbx::core::module_manager::destroy_all();
 
   return 0;
 }
