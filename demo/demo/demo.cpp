56b704ebe1f3d3282e3a420dc00712e00c1b2605
KabelitzJ <jonas-kabelitz@gmx.de>
2023-01-04 19:24:13 +0100

    sfafgasss

--- demo/demo/demo.cpp
+++ demo/demo/demo.cpp
@@ -4,17 +4,17 @@
 
 #include <libsbx/core/core.hpp>
 #include <libsbx/devices/devices.hpp>
+#include <libsbx/graphics/graphics.hpp>
 
 class demo_application : public sbx::core::application {
 
 public:
 
   demo_application(sbx::core::engine& engine)
-  : sbx::core::application{sbx::core::version{0, 1, 0}},
-    _engine{engine} {
+  : sbx::core::application{sbx::core::version{0, 1, 0}} {
     auto& window = sbx::devices::device_module::get().window();
 
-    window.set_on_window_closed([&engine](const sbx::devices::window_closed_event&){
+    window.set_on_window_closed([&engine]([[maybe_unused]] const sbx::devices::window_closed_event& event){
       engine.quit();
     });
   }
@@ -25,14 +25,11 @@ public:
     
   }
 
-  sbx::core::engine& _engine;
-
 }; // class demo_application
 
 auto main(int argc, char** argv) -> int {
   try {
-    auto args = std::vector<std::string_view>{argv, argv + argc};
-    auto engine = std::make_unique<sbx::core::engine>(std::move(args));
+    auto engine = std::make_unique<sbx::core::engine>(std::vector<std::string_view>{argv, argv + argc});
     auto application = std::make_unique<demo_application>(*engine);
     engine->run(application);
   } catch(const std::exception& exception) {
