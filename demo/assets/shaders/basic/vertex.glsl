f21e47972081a8cbde1a78e237d21d14d19e4c68
KabelitzJ <jonas-kabelitz@gmx.de>
2023-02-19 19:05:10 +0100

    adasdsa

--- demo/assets/shaders/basic/vertex.glsl
+++ demo/assets/shaders/basic/vertex.glsl
@@ -10,5 +10,5 @@ layout(location = 0) out vec4 vertex_out_color;
 
 void main() {
   gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
-  vertex_out_color = vec4(1.0, 1.0, 1.0, 1.0);
+  vertex_out_color = vec4(0.98, 0.29, 0.24, 1.0);
 }
