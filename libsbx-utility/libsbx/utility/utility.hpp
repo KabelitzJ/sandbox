73cb72c5630d7d68a498b28d2a4e0b62486bb0aa
KabelitzJ <jonas-kabelitz@gmx.de>
2023-03-13 20:44:59 +0100

    asda

--- libsbx-utility/libsbx/utility/utility.hpp
+++ libsbx-utility/libsbx/utility/utility.hpp
@@ -45,6 +45,7 @@
 #include <libsbx/utility/hash.hpp>
 #include <libsbx/utility/concepts.hpp>
 #include <libsbx/utility/fast_mod.hpp>
-#include <libsbx/utility/observer_ptr.hpp> 
+#include <libsbx/utility/observer_ptr.hpp>
+#include <libsbx/utility/overload.hpp>
 
 #endif // LIBSBX_UTILITY_HPP_
