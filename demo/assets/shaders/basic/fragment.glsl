574329a5e64acf5c5687ca52da079325ba8ef038
KabelitzJ <jonas-kabelitz@gmx.de>
2023-06-22 14:52:39 +0200

    sdsa

--- demo/assets/shaders/basic/fragment.glsl
+++ demo/assets/shaders/basic/fragment.glsl
@@ -5,57 +5,53 @@ layout(location = 1) in vec3 in_normal;
 
 layout(location = 0) out vec4 out_color;
 
-// layout(push_constant) uniform push_constant {
-//   vec4 ambient_color;
-//   vec4 diffuse_color;
-//   vec4 specular_color;
-//   vec4 shininess;
-//   vec4 camera_position;
-//   vec4 light_color;
-//   vec4 light_position;
-// } uniform_push_constant;
+const vec4 ambient_color = vec4(0.1, 0.1, 0.1, 1.0);
+const vec4 diffuse_color = vec4(0.5, 0.5, 0.5, 1.0);
+const vec4 specular_color = vec4(1.0, 1.0, 1.0, 1.0);
+const vec4 shininess = vec4(32.0, 0.0, 0.0, 0.0);
+const vec4 camera_position = vec4(2.0, 2.0, 1.0, 0.0);
+const vec4 light_position = vec4(-1.0, 3.0, 1.0, 0.0);
+const vec4 light_color = vec4(1.0, 1.0, 1.0, 1.0);
 
-// vec4 phong_shading(vec3 light_direction, float intensity) {
-//   // Calculate the ambient color
-//   vec4 ambient = uniform_push_constant.light_color * uniform_push_constant.ambient_color;
+vec4 phong_shading(vec3 light_direction, float intensity) {
+  // Calculate the ambient color
+  vec4 ambient = light_color * ambient_color;
 
-//   // Calculate the diffuse color
-//   vec4 diffuse = uniform_push_constant.light_color * uniform_push_constant.diffuse_color * intensity;
+  // Calculate the diffuse color
+  vec4 diffuse = light_color * diffuse_color * intensity;
 
-//   // Calculate the specular color
-//   vec3 camera_direction = normalize(vec3(uniform_push_constant.camera_position) - in_position);
-//   vec3 reflection_direction = reflect(-light_direction, in_normal);
-//   float specular_intensity = pow(max(dot(camera_direction, reflection_direction), 0.0), uniform_push_constant.shininess.x);
-//   vec4 specular = uniform_push_constant.light_color * uniform_push_constant.specular_color * specular_intensity;
+  // Calculate the specular color
+  vec3 camera_direction = normalize(vec3(camera_position) - in_position);
+  vec3 reflection_direction = reflect(-light_direction, in_normal);
+  float specular_intensity = pow(max(dot(camera_direction, reflection_direction), 0.0), shininess.x);
+  vec4 specular = light_color * specular_color * specular_intensity;
 
-//   // Calculate the final color
-//   return ambient + diffuse + specular;
-// }
+  // Calculate the final color
+  return ambient + diffuse + specular;
+}
 
-// vec4 cel_shading(vec3 light_direction, float intensity) {
-//   const int CEL_LEVELS = 4;
-//   const vec4 SHADOW_COLOR = vec4(0.0, 0.0, 0.0, 1.0);
+vec4 cel_shading(vec3 light_direction, float intensity) {
+  const int CEL_LEVELS = 4;
+  const vec4 SHADOW_COLOR = vec4(0.0, 0.0, 0.0, 1.0);
 
-//   // Calculate the index of the shade based on the intensity
-//   float shade_index = floor(intensity * float(CEL_LEVELS));
+  // Calculate the index of the shade based on the intensity
+  float shade_index = floor(intensity * float(CEL_LEVELS));
   
-//   // Calculate the color based on the shade index
-//   vec4 cel_color = mix(SHADOW_COLOR, uniform_push_constant.ambient_color, shade_index / float(CEL_LEVELS - 1));
+  // Calculate the color based on the shade index
+  vec4 cel_color = mix(SHADOW_COLOR, ambient_color, shade_index / float(CEL_LEVELS - 1));
   
-//   // Output the final color
-//   return cel_color;
-// }
+  // Output the final color
+  return cel_color;
+}
 
 void main() {
-  // vec3 light_direction = normalize(vec3(uniform_push_constant.light_position) - in_position);
-  // float intensity = max(dot(in_normal, light_direction), 0.0);
+  vec3 light_direction = normalize(vec3(light_position) - in_position);
+  float intensity = max(dot(in_normal, light_direction), 0.0);
  
-  // vec4 phong_shading = phong_shading(light_direction, intensity);
-  // vec4 cel_shading = cel_shading(light_direction, intensity);
+  vec4 phong_shading = phong_shading(light_direction, intensity);
+  vec4 cel_shading = cel_shading(light_direction, intensity);
 
-  // float mix_factor = 0.33;
+  float mix_factor = 0.33;
 
-  // out_color = mix(phong_shading, cel_shading, mix_factor);
-
-  out_color = vec4(1.0, 0.0, 1.0, 1.0);
+  out_color = mix(phong_shading, cel_shading, mix_factor);
 }
