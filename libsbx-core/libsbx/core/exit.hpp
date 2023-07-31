a65ee672387b66260529488cf186d06dd8f039e8
Jonas Kabelitz <jonas-kabelitz@gmx.de>
2023-07-31 15:31:52 +0200

    Create exit.hpp

---
+++ libsbx-core/libsbx/core/exit.hpp
@@ -0,0 +1,15 @@
+#ifndef LIBSBX_CORE_EXIT_HPP_
+#define LIBSBX_CORE_EXIT_HPP_
+
+#include <cinttypes>
+
+namespace sbx::core {
+
+struct exit {
+  inline static constexpr auto success = std::int32_t{0};
+  inline static constexpr auto failure = std::int32_t{0};
+}; // struct exit
+
+} // namespace sbx::core
+
+#endif // LIBSBX_CORE_EXIT_HPP_
