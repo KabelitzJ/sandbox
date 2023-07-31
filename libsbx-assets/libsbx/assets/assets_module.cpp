83a52bd19dac299e0f59aa473641f829f2dcd19e
KabelitzJ <jonas-kabelitz@gmx.de>
2023-07-31 22:57:51 +0200

    asdf

--- libsbx-assets/libsbx/assets/assets_module.cpp
+++ libsbx-assets/libsbx/assets/assets_module.cpp
@@ -9,11 +9,12 @@ assets_module::assets_module() {
 }
 
 assets_module::~assets_module() {
-  // [NOTE] KAJ 2023-07-31 : This kind of seems like a cyclic dependency. Need to figure out how to break it.
+  // [NOTE] KAJ 2023-07-31 : This kind of seems like a cyclic dependency. Need to figure out how to break it. 
   auto& graphics_module = graphics::graphics_module::get();
 
   auto& logical_device = graphics_module.logical_device();
 
+  // [NOTE] KAJ 2023-07-31 : Wait for all gpu operations to finish before unloading assets.
   logical_device.wait_idle();
 
   unload_assets();
