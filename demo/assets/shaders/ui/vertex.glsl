da8009cadb45031232d6ea02465869be73df3c9b
KabelitzJ <jonas-kabelitz@gmx.de>
2023-11-15 18:58:44 +0100

    Added scripts

--- demo/assets/shaders/ui/vertex.glsl
+++ demo/assets/shaders/ui/vertex.glsl
@@ -1,7 +1,11 @@
 #version 450
 
+struct glyph {
+  vec2 offset;
+  vec2 uv;
+};
+
 layout(location = 0) in vec2 in_position;
-layout(location = 1) in vec2 in_uv;
 
 layout(location = 0) out vec2 out_uv;
 
@@ -9,11 +13,13 @@ layout(binding = 0) uniform uniform_scene {
   mat4 projection;
 } scene;
 
-layout(binding = 1) buffer buffer_transforms {
-  mat4 data[];
-} transforms;
+layout(binding = 1) buffer buffer_glyphs {
+  glyph data[];
+} glyphs;
 
 void main() {
+  glyph glyph = glyphs.data[gl_InstanceIndex];
+
   out_uv = in_uv;
 
   gl_Position = scene.projection * transforms.data[gl_InstanceIndex] * vec4(in_position, 0.0, 1.0);
