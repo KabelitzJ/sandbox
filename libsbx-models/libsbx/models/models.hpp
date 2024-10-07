ea5cc7fe7451a9b58ff4f31d0fe07769aa7e5da1
KabelitzJ <jonas-kabelitz@gmx.de>
2024-10-07 13:12:05 +0200

    fsdökfödslk

--- libsbx-models/libsbx/models/models.hpp
+++ libsbx-models/libsbx/models/models.hpp
@@ -6,7 +6,7 @@
 #include <libsbx/models/vertex3d.hpp>
 #include <libsbx/models/mesh.hpp>
 
-#include <libsbx/models/mesh_subrenderer.hpp>
+#include <libsbx/models/static_mesh_subrenderer.hpp>
 
 #include <libsbx/models/loaders/obj_loader.hpp>
 #include <libsbx/models/loaders/gltf_loader.hpp>
