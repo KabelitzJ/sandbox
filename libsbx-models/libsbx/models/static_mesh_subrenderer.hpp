2b3f62b8df625e9b682c8ede6b61259efcc96938
KabelitzJ <jonas-kabelitz@gmx.de>
2025-07-25 21:31:48 +0200

    sdfdsdf

--- libsbx-models/libsbx/models/static_mesh_subrenderer.hpp
+++ libsbx-models/libsbx/models/static_mesh_subrenderer.hpp
@@ -60,7 +60,6 @@
 
 
 #include <libsbx/models/vertex3d.hpp>
-#include <libsbx/models/pipeline.hpp>
 #include <libsbx/models/mesh.hpp>
 
 namespace sbx::models {
@@ -90,6 +89,30 @@ class static_mesh_subrenderer final : public graphics::subrenderer {
 
   // inline static constexpr auto transparency_disabled = std::uint32_t{0u};
   // inline static constexpr auto transparency_enabled = std::uint32_t{1u};
+
+  class pipeline : public graphics::graphics_pipeline {
+
+    inline static const auto pipeline_definition = graphics::pipeline_definition{
+      .depth = graphics::depth::read_write,
+      .uses_transparency = false,
+      .rasterization_state = graphics::rasterization_state{
+        .polygon_mode = graphics::polygon_mode::fill,
+        .cull_mode = graphics::cull_mode::back,
+        .front_face = graphics::front_face::counter_clockwise
+      },
+      // .vertex_input = graphics::vertex_input<models::vertex3d>::description()
+    };
+
+    using base_type = graphics::graphics_pipeline;
+
+  public:
+
+    pipeline(const std::filesystem::path& path, const graphics::render_graph::graphics_pass& pass)
+    : base_type{path, pass, pipeline_definition} { }
+
+    ~pipeline() override = default;
+
+  }; // class pipeline
 
 public:
 
