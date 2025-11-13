d4c2952a429fa007dacd4a97bdfdbc6d222c57c0
Jonas Kabelitz <jonas.kabelitz@belden.com>
2025-11-13 17:17:58 +0100

    agsdga

---
+++ libsbx-post/libsbx/post/filters/selection_filter.hpp
@@ -0,0 +1,70 @@
+#ifndef LIBSBX_POST_SELECTION_FILTER_HPP_
+#define LIBSBX_POST_SELECTION_FILTER_HPP_
+
+#include <libsbx/core/profiler.hpp>
+
+#include <libsbx/scenes/scenes_module.hpp>
+
+#include <libsbx/post/filter.hpp>
+
+namespace sbx::post {
+
+class selection_filter final : public filter {
+
+  using base = filter;
+
+public:
+
+  selection_filter(const graphics::render_graph::graphics_pass& pass, const std::filesystem::path& path, const std::string& image, const std::string& object_id_image, const std::string& normalized_depth_image)
+  : base{pass, path},
+    _push_handler{base::pipeline()},
+    _image{image},
+    _object_id_image{object_id_image},
+    _normalized_depth_image{normalized_depth_image} { }
+
+  ~selection_filter() override = default;
+
+  auto render(graphics::command_buffer& command_buffer) -> void override {
+    SBX_PROFILE_SCOPE("selection_filter::render");
+
+    auto& scenes_module = core::engine::get_module<scenes::scenes_module>();
+    auto& scene = scenes_module.scene();
+
+    auto& graphics_module = core::engine::get_module<graphics::graphics_module>();
+
+    auto& pipeline = base::pipeline();
+    auto& descriptor_handler = base::descriptor_handler();
+
+    pipeline.bind(command_buffer);
+
+    _push_handler.push("color", sbx::math::color{1.0, 0.86, 0.49, 1.0});
+    _push_handler.push("thickness", 0.01f);
+
+    descriptor_handler.push("scene", scene.uniform_handler());
+    descriptor_handler.push("image", graphics_module.attachment(_image));
+    descriptor_handler.push("object_id_image", graphics_module.attachment(_object_id_image));
+    descriptor_handler.push("normalized_depth_image", graphics_module.attachment(_normalized_depth_image));
+
+    if (!descriptor_handler.update(pipeline)) {
+      return;
+    }
+
+    descriptor_handler.bind_descriptors(command_buffer);
+    _push_handler.bind(command_buffer);
+
+    command_buffer.draw(3, 1, 0, 0);
+  }
+
+private:
+
+  graphics::push_handler _push_handler;
+
+  std::string _image;
+  std::string _object_id_image;
+  std::string _normalized_depth_image;
+
+}; // class selection_filter
+
+} // namespace sbx::post 
+
+#endif // LIBSBX_POST_SELECTION_FILTER_HPP_
